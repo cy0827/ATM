28629ae8d9457274afe274424b7290b287f6506e
cy0827 <38992152+cy0827@users.noreply.github.com>
2022-11-20 20:51:42 +0100

    Commit 8 - Correzione errore sintattico

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import code.Business_logic.*;;
+import code.Business_logic.*;
 
 public class TestEuro {
     Euro testEuro;
